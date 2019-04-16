8c4aad28b6b764c03ec92556166808da616e4057
nataliavrg <ribeironatalia404@gmail.com>
2019-04-16 15:54:11 -0300

    v1

---
+++ src/app/componentes/compImagens/component.img.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+  selector: "component-img",
+  templateUrl: "component.img.html",
+  styleUrls: ["component.img.css"]
+})
+
+export class ComponentImg{
+  titulo: string = "Seriado";
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ComponentImg } from '../componentes/compImagens/component.img';
 
 import { HomePage } from './home.page';
 
@@ -18,6 +19,6 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, ComponentImg]
 })
 export class HomePageModule {}
